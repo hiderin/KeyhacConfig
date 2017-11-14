3e73f5026c87f329e69717db5d19098927244259
Naohide-aki-Pc <naojupiter1@gmail.com>
2017-11-14 09:04:02 +0900

    gvimの指定を相対パスに変更した。

--- call_gitbash_gvim.bat
+++ call_gitbash_gvim.bat
@@ -1,5 +1,5 @@
 @echo off
 
-sh --login -i -c "c:\\dropbox\\mysys\\portvim\\gvim.exe -c ':VimFilerDouble'"
+sh --login -i -c "../portvim/gvim.exe -c ':VimFilerDouble'"
 rem sh --login -i call_gvim.sh
 
